c2d04d68737efddf752333403caa7d8798f4332b
RyanO97 <oromeo10@gmail.com>
2024-01-13 12:48:19 -0500

    initial command implementation

---
+++ index.js
@@ -0,0 +1,45 @@
+// Require the necessary discord.js classes
+const { Client, Events, Collection, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+const fs = require('node:fs');
+const path = require('node:path');
+
+
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+
+client.commands = new Collection();
+// Create a new client instance
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(token);
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
