387d8ba55d2dc446674bc8cc295d17dfb31481c0
RyanO97 <oromeo10@gmail.com>
2024-09-07 23:35:14 -0400

    index lint

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const path = require('node:path');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 // It makes some properties non-nullable.
